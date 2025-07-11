b2811342ce4abf2df56eba6716e6cc3490dbdc12
Luis Maria <gamerluis.m@gmail.com>
2025-07-11 22:35:44 +0000

    add AccountDAO class and methods

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,57 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+    public Account getAccount(String username, String password) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username=? and password=?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Account userAccount = new Account();
+                userAccount.setAccount_id(rs.getInt("account_id"));
+                userAccount.setUsername(rs.getString("username"));
+                userAccount.setPassword(rs.getString("password"));
+                
+                return userAccount;
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account createAccount(String username, String password) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username=?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+                ps = connection.prepareStatement(sql);
+                ps.setString(1, username);
+                ps.setString(2, password);
+                if (ps.executeUpdate() == 1) {
+                    return getAccount(username, password);
+                }
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
