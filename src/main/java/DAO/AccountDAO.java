96fc72b2b2806154c355f4f89badf833da56ad9c
Luis Maria <gamerluis.m@gmail.com>
2025-07-12 02:14:10 +0000

    switch AccountDAO methods to boolean return types and added a check method

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -4,53 +4,56 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import Model.Account;
 import Util.ConnectionUtil;
 
 public class AccountDAO {
-    public Account getAccount(String username, String password) {
+    public boolean getAccountIdByUserAndPass(Account account) {
         Connection connection = ConnectionUtil.getConnection();
         try {
             String sql = "SELECT * FROM account WHERE username=? and password=?";
             PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, username);
-            ps.setString(2, password);
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                return new Account(
-                    rs.getInt("account_id"),
-                    rs.getString("username"),
-                    rs.getString("password")
-                );
+                account.setAccount_id(rs.getInt("account_id"));
+                return true;
             }
         }
         catch(SQLException e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        return false;
     }
 
-    public boolean createAccount(Account account) {
+    public boolean checkAccountExists(Account account) {
         Connection connection = ConnectionUtil.getConnection();
         try {
             String sql = "SELECT * FROM account WHERE username=?";
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, account.getUsername());
             ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                sql = "INSERT INTO account (username, password) VALUES (?, ?)";
-                ps = connection.prepareStatement(sql);
-                ps.setString(1, account.getUsername());
-                ps.setString(2, account.getPassword());
-                if (ps.executeUpdate() == 1) {
-                    rs = ps.getGeneratedKeys();
-                    if (rs.next()) {
-                        account.setAccount_id(
-                            rs.getInt("account_id")
-                        );
-                    }
+            return rs.next();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean insertAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+            if (ps.executeUpdate() == 1) {
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    account.setAccount_id(rs.getInt("account_id"));
                     return true;
                 }
             }
